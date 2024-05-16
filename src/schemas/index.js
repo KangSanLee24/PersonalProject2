a3f1f1c35a2a4d15bc8746fa0f229cc7368bc9ba
이강산 <gangsan1414@gmail.com>
2024-05-16 19:38:20 +0900

    .env파일만들고 테스트완료

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,15 +1,14 @@
 // /src/schemas/index.js
 
 import mongoose from "mongoose";
+import { MONGO_URL } from "./env.js";
 
+console.log("몽고URL", MONGO_URL); //
 const connect = () => {
   mongoose
-    .connect(
-      "mongodb+srv://sparta-user:asdf4321@cluster0.adaavxh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        dbName: "pp2marketplace", // pp2marketplace 데이터베이스명을 사용합니다.
-      }
-    )
+    .connect(MONGO_URL, {
+      dbName: "pp2marketplace", // pp2marketplace 데이터베이스명을 사용합니다.
+    })
     .then(() => console.log("몽고디비 연결 성공"))
     .catch((err) => console.log(`몽고디비 연결 실패 ${err}`));
 };
