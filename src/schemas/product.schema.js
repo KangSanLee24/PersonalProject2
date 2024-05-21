eb1e0f51b3c90029dd4ef5db321c82af9cc20c32
이강산 <gangsan1414@gmail.com>
2024-05-21 20:37:16 +0900

    id값삭제, createdAt,updateAt대신 timestamps사용

--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -1,44 +1,37 @@
-// /schemas/products.schema.js
-
 import mongoose from "mongoose";
+import { PRODUCT_STATUS } from "../constants/product.constant";
 
 // 상품(goods)에 대한 정보를 나타내는 스키마를 정의합니다.
-const ProductsSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true, // order 필드 또한 필수 요소입니다.
-    unique: true,
+const ProductsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String, // 상품의 이름을 나타냅니다.
+      required: true, // 필수 항목입니다.
+    },
+    description: {
+      type: String, // 상품의 상세 설명을 나타냅니다.
+      required: false,
+    },
+    manager: {
+      type: String, // 상품의 카테고리를 나타냅니다.
+      required: true, // 필수 항목입니다.
+    },
+    password: {
+      type: String, // 상품의 이름을 나타냅니다.
+      required: true, // 필수 항목입니다.
+      select: false,
+    },
+    status: {
+      type: String, // 상품의 상태를 나타냅니다.
+      required: true, // 필수 항목입니다.
+      enum: Object.values(PRODUCT_STATUS),
+      default: PRODUCT_STATUS.FOR_SALE,
+    },
   },
-  name: {
-    type: String, // 상품의 이름을 나타냅니다.
-    required: true, // 필수 항목입니다.
-  },
-  description: {
-    type: String, // 상품의 상세 설명을 나타냅니다.
-    required: false,
-  },
-  manager: {
-    type: String, // 상품의 카테고리를 나타냅니다.
-    required: true, // 필수 항목입니다.
-  },
-  password: {
-    type: String, // 상품의 이름을 나타냅니다.
-    required: true, // 필수 항목입니다.
-  },
-  status: {
-    type: String, // 상품의 상태를 나타냅니다.
-    required: true, // 필수 항목입니다.
-    enum: ["FOR_SALE", "SOLD_OUT"],
-  },
-  createdAt: {
-    type: Date, // 상품이 생성된 시간을 나타냅니다.
-    required: true, // 필수 항목입니다.
-  },
-  updateAt: {
-    type: Date, // 상품이 수정된 가장 최근 시간을 나타냅니다.
-    required: false,
-  },
-});
+  // timestamps는 몽구스스키마에 찾아보면 있고,
+  // toJSON을 호출, 가상으로 만든 속성(_id)
+  { timestamps: true, toJSON: { virtuals: true } }
+);
 
 // 위에서 정의한 스키마를 이용하여 'Products'라는 이름의 모델을 생성합니다.
-export default mongoose.model("Products", ProductsSchema);
+export const Product = mongoose.model("Product", ProductsSchema);
